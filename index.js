3f7622ff00325153c05d3c01c23da7d80c518505
Mateo Sancho <mat.sancho.e@gmail.com>
2025-10-08 16:40:13 +0200

    Start

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Iteration 1: Names and Input
+let hacker1 = "Mateo";
+console.log(`The driver's name is ${hacker1}`)
 
+let hacker2 = "YYYY";
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
-
+let name1 = hacker1;
+name1 = name1
+let name2 = hacker2;
+name2 = name2
 
 // Iteration 3: Loops
