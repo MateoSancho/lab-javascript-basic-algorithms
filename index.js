7da079d5669381c7238a12f9b4d2648aec27924b
Mateo Sancho <mat.sancho.e@gmail.com>
2025-10-08 17:34:18 +0200

    Finished

--- index.js
+++ index.js
@@ -2,13 +2,55 @@
 let hacker1 = "Mateo";
 console.log(`The driver's name is ${hacker1}`)
 
-let hacker2 = "YYYY";
+let hacker2 = "Naim";
 console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
-let name1 = hacker1;
-name1 = name1
-let name2 = hacker2;
-name2 = name2
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+}
 
 // Iteration 3: Loops
+//3.1
+let nameUpperCase = "";
+for (let i = 0; i < hacker1.length; i++) {
+    if (i === hacker1.length - 1) {
+        nameUpperCase += hacker1[i].toUpperCase();
+    } else {
+        nameUpperCase += hacker1[i].toUpperCase() + " "
+    }
+}
+console.log(nameUpperCase)
+
+//3.2
+let nameReverse = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    nameReverse += hacker2[i]
+}
+console.log(nameReverse)
+
+//3.3
+let abc = "abcdefghijklmnopqrstuvwxyz";
+let numName;
+let numName2;
+for (let a = 0; a < abc.length; a++) {
+    if (hacker1[0].toLowerCase() === abc[a]) {
+        numName = a
+    } else if (hacker2[0].toLowerCase() === abc[a]) {
+        numName2 = a
+    }
+
+}
+
+if (numName < numName2) {
+        console.log("The driver's name goes first.")
+    } else if (numName > numName2) {
+        console.log("Yo, the navigator goes first, definitely.")
+    } else {
+        console.log("What?! You both have the same name?")
+    }
+
